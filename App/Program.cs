2b12258225ecf84094f6721eae0e97be58c51422
VitorNuness <vitor1nuness@gmail.com>
2024-09-02 19:40:15 -0300

    removing dead classes and adding static files

--- App/Program.cs
+++ App/Program.cs
@@ -1,86 +1,89 @@
-using System.Text;
-using App.Repositories.Database;
-using App.Repositories;
-using App.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using App.Repositories.Database;
+using App.Repositories;
+using App.Services;
+using System.Text;
 
-internal class Program
+namespace App
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-        builder.Services.AddDbContext<CheckoutDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-        builder.Services.AddCors(opt =>
-            {
-                opt.AddPolicy("AllowAnyOrigins",
-                builder =>
-                    {
-                        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-                    });
-            });
-
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                        .AddJwtBearer(options =>
-                        {
-                            options.TokenValidationParameters = new TokenValidationParameters
-                            {
-                                ValidateIssuer = false,
-                                ValidateAudience = false,
-                                ValidateLifetime = true,
-                                ValidateIssuerSigningKey = true,
-                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SECRET_KEY"]!))
-                            };
-                        });
-
-        builder.Services.AddAuthorization();
-
-        builder.Services
-            .AddControllers()
-            .AddJsonOptions(option =>
-                option.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
-
-        builder.Services.AddScoped<CampaignService>();
-        builder.Services.AddScoped<CategoryService>();
-        builder.Services.AddScoped<ImageService>();
-        builder.Services.AddScoped<OrderItemService>();
-        builder.Services.AddScoped<OrderService>();
-        builder.Services.AddScoped<ProductService>();
-        builder.Services.AddScoped<SuggestionService>();
-        builder.Services.AddScoped<TokenService>();
-        builder.Services.AddScoped<UserService>();
-
-        builder.Services.AddScoped<CampaignRepository>();
-        builder.Services.AddScoped<CategoryRepository>();
-        builder.Services.AddScoped<ImageRepository>();
-        builder.Services.AddScoped<OrderItemRepository>();
-        builder.Services.AddScoped<OrderRepository>();
-        builder.Services.AddScoped<ProductRepository>();
-        builder.Services.AddScoped<UserRepository>();
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
+        static void Main(string[] args)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<CheckoutDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            builder.Services.AddCors(opt =>
+                {
+                    opt.AddPolicy("AllowAnyOrigins",
+                    builder =>
+                        {
+                            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                        });
+                });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                            .AddJwtBearer(options =>
+                            {
+                                options.TokenValidationParameters = new TokenValidationParameters
+                                {
+                                    ValidateIssuer = false,
+                                    ValidateAudience = false,
+                                    ValidateLifetime = true,
+                                    ValidateIssuerSigningKey = true,
+                                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SECRET_KEY"]!))
+                                };
+                            });
+
+            builder.Services.AddAuthorization();
+
+            builder.Services
+                .AddControllers()
+                .AddJsonOptions(option =>
+                    option.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
+
+            builder.Services.AddScoped<CampaignService>();
+            builder.Services.AddScoped<CategoryService>();
+            builder.Services.AddScoped<FileService>();
+            builder.Services.AddScoped<OrderItemService>();
+            builder.Services.AddScoped<OrderService>();
+            builder.Services.AddScoped<ProductService>();
+            builder.Services.AddScoped<SuggestionService>();
+            builder.Services.AddScoped<TokenService>();
+            builder.Services.AddScoped<UserService>();
+
+            builder.Services.AddScoped<CampaignRepository>();
+            builder.Services.AddScoped<CategoryRepository>();
+            builder.Services.AddScoped<OrderItemRepository>();
+            builder.Services.AddScoped<OrderRepository>();
+            builder.Services.AddScoped<ProductRepository>();
+            builder.Services.AddScoped<UserRepository>();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.UseCors("AllowAnyOrigins");
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.UseCors("AllowAnyOrigins");
-
-        app.Run();
     }
 }
