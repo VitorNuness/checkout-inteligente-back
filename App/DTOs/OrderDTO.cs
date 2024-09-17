0b505b397faaf261281aed0d2d6fe42347e5f2b4
VitorNuness <vitor1nuness@gmail.com>
2024-09-17 10:50:39 -0300

    refactor order DTO adding status

--- App/DTOs/OrderDTO.cs
+++ App/DTOs/OrderDTO.cs
@@ -9,6 +9,7 @@ namespace App.DTOs
         public int Id { get; set; } = order.Id;
         public IEnumerable<OrderItemDTO?> Items { get; set; } = order.Items?.Select(i => new OrderItemDTO(i)) ?? [];
         public double TotalAmount { get; set; } = order.TotalAmount;
+        public string Status { get; set; } = order.Status.ToString();
         public int UserId { get; set; } = order.User.Id;
     }
 }
