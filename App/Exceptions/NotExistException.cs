a83fadae53dc5b4a574e2c53af1f4697c5bebe59
VitorNuness <vitor1nuness@gmail.com>
2024-09-17 10:29:32 -0300

    creating not found exception

---
+++ App/Exceptions/NotExistException.cs
@@ -0,0 +1,13 @@
+namespace App.Exceptions
+{
+    [Serializable]
+    public class NotExistException : Exception
+    {
+        public NotExistException() { }
+        public NotExistException(string message) : base(message) { }
+        public NotExistException(string message, Exception inner) : base(message, inner) { }
+        protected NotExistException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
