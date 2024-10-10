5a5b9b1eaddc32db9775fe960375bce5ba6dd6fe
VitorNuness <vitor1nuness@gmail.com>
2024-10-09 21:02:29 -0300

    adicionando anotação para parâmetro obrigatório na requisição

--- App/DTOs/CampaignInputDTO.cs
+++ App/DTOs/CampaignInputDTO.cs
@@ -1,8 +1,10 @@
 namespace App.DTOs;
 
+using System.Text.Json.Serialization;
+
 public class CampaignInputDTO
 {
     public string? Title { get; set; }
-    public bool Active { get; set; }
+    [JsonRequired] public bool Active { get; set; }
     public List<int>? ProductsId { get; set; }
 }
