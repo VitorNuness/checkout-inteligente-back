cc032bc7549fb6f970bb30be53a6f1e2f41f17e6
VitorNuness <vitor1nuness@gmail.com>
2024-10-09 21:04:22 -0300

    adicionando anotações para os parâmetros Quantidade e Preço na requisição

--- App/DTOs/ProductInputDTO.cs
+++ App/DTOs/ProductInputDTO.cs
@@ -1,9 +1,11 @@
 namespace App.DTOs;
 
+using System.Text.Json.Serialization;
+
 public class ProductInputDTO
 {
-    public string Name { get; set; }
-    public double Quantity { get; set; }
-    public double Price { get; set; }
+    public string? Name { get; set; }
+    [JsonRequired] public double Quantity { get; set; }
+    [JsonRequired] public double Price { get; set; }
     public int CategoryId { get; set; }
 }
