c11c48baa459d1ab278c7fd75e347db600110a60
VitorNuness <vitor1nuness@gmail.com>
2024-11-26 22:09:58 -0300

    adicionando teste de adicionar produto no carrinho

--- src/Checkout/Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
+++ src/Checkout/Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49299956caad73958589b21c2cae938d5d1b9f96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ed78004ef1e244455021b6ab242de48841f355")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/Checkout/Integrations/Frontend.Test/Program.cs
+++ tests/Checkout/Integrations/Frontend.Test/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Frontend.Test;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://localhost:5173/signup");
+
+        void TestUser(int i)
+        {
+
+
+            var fieldName = driver.FindElement(By.Id("name"));
+            fieldName.Clear();
+            fieldName.SendKeys("Joao");
+
+            var fieldEmail = driver.FindElement(By.Id("email"));
+            fieldEmail.Clear();
+            fieldEmail.SendKeys("teste@aaaa.com");
+
+            var filedEmailConfirmation = driver.FindElement(By.Id("emailConfirmation"));
+            filedEmailConfirmation.Clear();
+            filedEmailConfirmation.SendKeys("teste@aaaa.com");
+
+            var fieldPassword = driver.FindElement(By.Id("password"));
+            fieldPassword.Clear();
+            fieldPassword.SendKeys("abcd1234");
+
+            var fieldPasswordConfirmation = driver.FindElement(By.Id("passwordConfirmation"));
+            fieldPasswordConfirmation.Clear();
+            fieldPasswordConfirmation.SendKeys("abcd1234");
+
+            var fieldCheckBox = driver.FindElement(By.Id("terms"));
+            fieldCheckBox.Click();
+
+
+            var buttonGravar = driver.FindElement(By.Id("btnGravar"));
+            buttonGravar.Click();
+
+            Thread.Sleep(1000);
+
+
+            var botaoMouse = driver.FindElement(By.CssSelector("#app > div.mais-vendidos-container > div > div:nth-child(3) > div > div > div.card-body.justify-content-between > button"));
+            botaoMouse.Click();
+
+            Thread.Sleep(1000);
+
+
+            var botaoCarrinho = driver.FindElement(By.CssSelector("#app > header > div > nav > div > div:nth-child(2) > ul > div > li:nth-child(1) > button"));
+            botaoCarrinho.Click();
+
+            Thread.Sleep(1500);
+
+            var botaoFinal = driver.FindElement(By.Id("btnFinalizar"));
+            botaoFinal.Click();
+
+        }
+        for (var i = 0; i < 1; i++)
+        {
+            TestUser(i);
+
+        }
+        //driver.Quit();
+    }
+}
